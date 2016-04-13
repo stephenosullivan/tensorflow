274a01963499c0ab1e46979e7da31683bb0ee035
Stephen O'Sullivan <osullisg@gmail.com>
2016-04-13 15:13:02 -0700

    Initial commit from local git

---
+++ Tensorflow/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tensorflow
+//
+//  Created by Stephen O'Sullivan on 4/13/16.
+//  Copyright (c) 2016 Stephen O'Sullivan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
